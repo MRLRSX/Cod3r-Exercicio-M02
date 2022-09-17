ab31b842b2277fa2c421ee2ad3eff14652f045f2
MRLRSX <lucas.rocha-dev@outlook.com>
2022-09-17 09:34:15 -0300

    DEFAULT AND STATIC METHOD INTERFACE

--- src/exercicioUm/Calculo.java
+++ src/exercicioUm/Calculo.java
@@ -1,6 +1,15 @@
 package exercicioUm;
 
+@FunctionalInterface
 public interface Calculo {
    
 	public abstract double executar(double a, double b);
+	
+	default String legal() {
+		return "Exemplo: Default method Interface";
+	}
+	
+	static String muitoLegal() {
+		return "Muito Legal";
+	}
 }
--- src/exercicioUm/Program_02.java
+++ src/exercicioUm/Program_02.java
@@ -1,11 +1,13 @@
 package exercicioUm;
 
+/**@author LUCAS ROCHA */
+
 public class Program_02 {
 	public static void main(String[] args) {
 
 		/**
 		 *  POLIMORFISMO USANDO INTERFACE E LAMBDA
-		 *  
+		 *  DEFAULT METHOD
 		 * */
 		
 		Calculo soma = (x, y) -> { return x + y; };
@@ -14,6 +16,9 @@ public class Program_02 {
 		soma = (x, y) -> x * y ;
 		System.out.println(soma.executar(5.0, 3.0));
 		
+		System.out.println(soma.legal());
+		
+		System.out.println(Calculo.muitoLegal());
 		
 	}
 }
